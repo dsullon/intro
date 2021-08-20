26ae088d5f924eede31d9d738d27c6a64fe17099
Alex Sullon <dsullon@grupolarepublica.com.pe>
2021-08-20 12:24:53 -0500

    Creación de archivo dart

---
+++ tipos.dart
@@ -0,0 +1,30 @@
+void main() {
+  // Tipo entero
+  int numero_empleados = 10;
+
+  // Tipo string
+  String nombre_empleado = "José Campos";
+
+  // Tipo double
+  double tipo_cambio = 3.5;
+
+  print(numero_empleados);
+  print(nombre_empleado);
+
+  double monto_soles = 5000;
+
+  double monto_dolares = monto_soles / tipo_cambio;
+
+  double MontoDolares = 300;
+  double montoDolares = 500;
+
+  print(MontoDolares);
+  print(montoDolares);
+
+  bool esUsuario = false;
+
+  List<String> empleados = List.empty();
+  empleados.add("Pedro");
+  empleados.add("30");
+  empleados.add("false");
+}
